7a5fdf6539923eb441bca9415d5573c662636cab
Leonel Cruz Peralta <leonelcruzperalta@gmail.com>
2026-07-31 20:16:07 -0600

    Actualizar version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.logransoftware.telefonojuguete"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
     }
 
